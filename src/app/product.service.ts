9c54e33f10b5c2000b822b5265bceff6a1c9673c
makanab <makanabrian1@gmail.com>
2020-09-28 13:55:57 +0300

    http fix2

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,12 +5,12 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
-  private _ulbumUrl:string ="../assets/album.json";
+  private _albumUrl:string ="../assets/album.json";
 
   constructor(private _http:Http) { }
 
   getAlbum(id:number){
-    return this._http.get(this._ulbumUrl)
+    return this._http.get(this._albumUrl)
     .map((response)=>response.json());
 
   }
