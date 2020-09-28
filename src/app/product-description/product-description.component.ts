1acdcded9269fa44c34d2bbf09e8fcd42045ba45
makanab <makanabrian1@gmail.com>
2020-09-28 14:00:49 +0300

    product desc fix

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'app/product.service';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-description',
@@ -15,10 +15,8 @@ export class ProductDescriptionComponent implements OnInit {
   ngOnInit() {
 
     this._productService.getAlbum(1).subscribe(
-      response=>{
-        this.albumInfo = response;
-        console.log(response);
-      }
+      response=>   this.albumInfo = response
+
     )
 
   }
